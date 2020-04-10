71250265dd3b8e5eafb06ff797e8db7d39f84512
zhenhualee <lizhenhua@newland.com.cn>
2020-04-10 14:15:26 +0800

    添加注释

--- main.py
+++ main.py
@@ -12,31 +12,32 @@ from selenium.webdriver.common.by import By
 
 class TestTest():
     def setup_method(self, method):
-        # 复用浏览器
+        # 设置 ChromeOptions
         chrome_opts = webdriver.ChromeOptions()
+        '''
+        设置 chrome debugging代理:    
+        1.配置 chrome 浏览器环境变量： export PATH=$PATH:/Applications/Google\ Chrome.app/Contents/MacOS  
+        2.命令行cmd： Google\ Chrome -remote-debugging-port=9222
+        '''
         chrome_opts.debugger_address = '127.0.0.1:9222'
         self.driver = webdriver.Chrome(options=chrome_opts)
         # self.driver.get("https://work.weixin.qq.com")
-        self.driver.implicitly_wait(5)
+        # 设置 隐式等待3s
+        self.driver.implicitly_wait(3)
 
     def teardown_method(self, method):
         self.driver.quit()
 
-    """
-    1. 配置 chrome 浏览器环境变量
-    2. 启动 chrome debugging 时所有浏览器必须关闭
-    3. cmd: Google\ Chrome -remote-debugging-port=9222
-    4. 启动浏览器后扫码登录
-    4. 执行用例并获取 cookies 
-    """
-
     def test_cookies(self):
 
         # sleep(20)
+        # 获取cookies
         # cookies = self.driver.get_cookies()
         # print(cookies)
+        # 写入cookies.txt文件
         # with open("cookies.txt", 'w') as f:
         #     json.dump(cookies, f)
+        # 读取 cookies
         with open("cookies.txt", 'r') as f:
             cookies: List[Dict] = json.load(f)
 
