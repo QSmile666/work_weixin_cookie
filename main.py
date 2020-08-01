25b5e1331c53e161bd439c61b07d597d19847251
QSmile666 <59075552+QSmile666@users.noreply.github.com>
2020-08-01 13:04:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,5 +50,4 @@ class TestTest():
         self.driver.find_element(By.CSS_SELECTOR, '#username').send_keys("hello")
         self.driver.find_element(By.CSS_SELECTOR, '#memberAdd_acctid').send_keys("hello2")
         sleep(3)
-
-        #hhhh
+        
